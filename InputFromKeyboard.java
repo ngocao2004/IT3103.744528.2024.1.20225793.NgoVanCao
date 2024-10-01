9d4f2efcac3365d94e8695f55b21e1c5db578cb8
ngocao2004 <cao.nv225793@sis.hust.edu.vn>
2024-10-01 15:04:30 +0700

    lab1

---
+++ InputFromKeyboard.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class InputFromKeyboard {
+    public static void main(String[] args) {
+        Scanner keyBoard = new Scanner(System.in);
+        System.out.println("What's your name?");
+        String strName = keyBoard.nextLine();
+        System.out.println("How old are you?");
+        int iAge = keyBoard.nextInt();
+        System.out.println("How tall are you(m)? ");
+        Double dHeight = keyBoard.nextDouble();
+        System.out.println("Mrs/mr" + strName + " ," + iAge + " year old." + "Your height is " + dHeight);
+    }
+}
---
+++ Triangle.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class bai6_3 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int d = 1;
+        while(d<=n){
+            for(int i= d;i<n;i++){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=(2*d-1);k++){
+                System.out.print("*");
+            }
+            System.out.print("\n");
+            d++;
+        }
+    }
+}

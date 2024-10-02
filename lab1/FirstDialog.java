d4954fa65cb6bb97532409d6dca674d9458167ba
ngocao2004 <cao.nv225793@sis.hust.edu.vn>
2024-10-02 18:43:15 +0700

    Delete lab1 directory

--- lab1/FirstDialog.java
+++
@@ -1,9 +0,0 @@
-import java.awt.Component;
-import javax.swing.JOptionPane;
-
-public class FirstDialog {
-    public static void main(String[] args) {
-        JOptionPane.showMessageDialog((Component)null, "Hello World! How are you?");
-        System.exit(0);
-    }
-}

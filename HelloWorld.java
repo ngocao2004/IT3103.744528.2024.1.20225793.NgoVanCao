94a18b9e359a76dd2dbfa3d59f84abb4bafe810f
ngocao2004 <cao.nv225793@sis.hust.edu.vn>
2024-10-02 07:30:30 +0700

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,8 +0,0 @@
-//Example 1: HelloWorld.java
-//text-printting program
-public class HelloWorld {
-    public static void main(String[] args ){
-        System.out.println("xin chao \n cac ban!");
-        System.out.println("Hello \t world!");
-    }
-}
